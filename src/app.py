b33e104e3a6c6090e64a3d8c2e07c2877ee8e45a
Justin Pounders <justin.pounders@generalassemb.ly>
2017-12-22 13:37:40 -0500

    added user login and registration pages

--- src/app.py
+++ src/app.py
@@ -1,12 +1,52 @@
 import json
 import requests
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, render_template, session
+
+from common.database import Database
+from models.user import User
 
 app = Flask(__name__)
+app.secret_key = "justin"
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def home_template():
+    return render_template('home.html')
+
+@app.route('/login')
+def login_template():
+    return render_template('login.html')
+
+@app.route('/register')
+def register_template():
+    return render_template('register.html')
+
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+@app.route('/auth/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login_valid(email, password):
+        User.login(email)
+    else:
+        session['email'] = None
+
+    return render_template('profile.html', email=session['email'])
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
+
+    return render_template('profile.html', email=session['email'])
+
+
 
 @app.route('/message', methods=['GET', 'POST'])
 def message():
--- src/models/user.py
+++ src/models/user.py
@@ -1,10 +1,13 @@
 import uuid
+
+from flask import session
+
 from common.database import Database
 
 class User(object):
     def __init__(self, email, password, _id=None):
         self.email = email
-        self.password
+        self.password = password
         self._id = uuid.uuid4().hex if _id is None else _id
 
     @classmethod
@@ -21,14 +24,14 @@ class User(object):
 
 
     @staticmethod
-    def login_valid(self, email, password):
+    def login_valid(email, password):
         user = User.get_by_email(email)
         if user is not None:
             return user.password == password
         return False
 
     @classmethod
-    def register(self, email, password):
+    def register(cls, email, password):
         user = User.get_by_email(email)
         if user is None:
             # create user
